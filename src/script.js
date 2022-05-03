e1fc1985aaa2ba61c717550c5397e75a0b6f1591
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-05-03 16:54:35 +0200

    Add some shaders

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
+import vertexSource from "./vertexShader.glsl";
+import fragmentSource from "./fragmentShader.glsl";
+
 const main = () => {
   const canvas = document.getElementById("canvas");
   const gl = canvas.getContext("webgl");
-
   gl.clearColor(1, 0, 0, 1);
+  
+  const program = gl.createProgram();
 
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
---
+++ src/vertexShader.glsl
@@ -0,0 +1,5 @@
+attribute vec2 aCoordinate;
+
+void main() {
+  gl_Position = vec4(aCoordinate.xy, 0.0, 0.0);
+}
