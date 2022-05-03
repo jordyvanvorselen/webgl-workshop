2be41052b3c7dbe04dd52229b7a6bb3a1911c47f
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2022-05-03 17:01:56 +0200

    Add shader compilation and error collection

--- src/script.js
+++ src/script.js
@@ -1,12 +1,29 @@
 import vertexSource from "./vertexShader.glsl";
 import fragmentSource from "./fragmentShader.glsl";
 
+const createShader = (gl, type, source) => {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    source.split("\n").forEach((line, index) => {
+      console.log(index + 1, line);
+    });
+    throw new Error(gl.getShaderInfoLog(shader));
+  }
+
+  return shader;
+}
+
 const main = () => {
   const canvas = document.getElementById("canvas");
   const gl = canvas.getContext("webgl");
   gl.clearColor(1, 0, 0, 1);
   
   const program = gl.createProgram();
+
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
 
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
